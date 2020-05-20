9a896a0be89883e1e4bc86fc04034110cec928ea
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-05-20 09:52:09 -0400

    Cleanup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,6 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update]
   
   def index
-    # @users = User.all
     @users = User.paginate(page: params[:page], per_page: 5)
   end
   def show
