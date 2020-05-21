11869e36bc7976ea8146df19a6e4ae003d7c39ef
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-05-21 15:05:25 -0400

    Add test for name field requirement

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -5,4 +5,18 @@ class CategoryTest < ActiveSupport::TestCase
     @category = Category.new(name: "Sports")    
     assert @category.valid?
   end
+
+  test "name should be present" do
+    @category = Category.new(name: " ")    
+    assert_not @category.valid?
+  end
+
+  test "name should be unique" do
+  end
+
+  test "name should not be too long" do
+  end
+
+  test "name should not be too short" do
+  end
 end
