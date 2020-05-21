d83c7ca3f1a9a3886a314fb0fa4ff269c03e8a67
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-05-20 21:17:45 -0400

    Add helper method to render alert style type

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def alert_style_type(type)
+    if type == 'notice'
+      return 'alert-success' 
+    elsif type == 'alert'
+      return 'alert-danger'
+    end
+  end
 end
