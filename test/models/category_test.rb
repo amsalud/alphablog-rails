03b1f96850b4440ef57157ccff9def8b4480c9b5
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-05-21 15:11:11 -0400

    Add test for min and max characters for category name

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -21,8 +21,12 @@ class CategoryTest < ActiveSupport::TestCase
   end
 
   test "name should not be too long" do
+    @category.name = "a" * 26
+    assert_not @category.valid?
   end
 
   test "name should not be too short" do
+    @category.name = "aa"
+    assert_not @category.valid?
   end
 end
