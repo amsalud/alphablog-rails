0b899cbd9676db2839bee42750a637d1d31b3d22
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-05-21 21:52:57 -0400

    Eager load categories with articles query

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,7 +7,7 @@ class ArticlesController < ApplicationController
   end
 
   def index
-    @articles = Article.paginate(page: params[:page], per_page: 5)
+    @articles = Article.eager_load(:categories).paginate(page: params[:page], per_page: 5)
   end
 
   def new
