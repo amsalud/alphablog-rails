25e1678bad8b2737e39ab3fa939673a648894c76
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-05-21 22:23:53 -0400

    Add functionality to update category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,5 @@
 class CategoriesController < ApplicationController
-  before_action :set_category, only: [:show]
+  before_action :set_category, only: [:show, :edit, :update]
   before_action :require_user, except: [:show, :index]
   before_action :require_admin, except: [:show, :index]
 
@@ -23,6 +23,18 @@ class CategoriesController < ApplicationController
   
   def show
     @articles = @category.articles.paginate(page: params[:page], per_page: 5)
+  end
+
+  def edit
+  end
+
+  def update
+    if @category.update(category_params)
+      flash[:notice] = "Category was edited successfully"
+      redirect_to @category
+    else
+      render 'edit'
+    end
   end
 
   private
