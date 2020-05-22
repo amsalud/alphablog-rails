99d375832173b2b4a19d33de94ff1fb3972b38b9
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-05-21 21:17:03 -0400

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_21_185809) do
+ActiveRecord::Schema.define(version: 2020_05_22_011021) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -18,6 +23,11 @@ ActiveRecord::Schema.define(version: 2020_05_21_185809) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer "user_id"
+  end
+
+  create_table "articles_categories", id: false, force: :cascade do |t|
+    t.integer "category_id", null: false
+    t.integer "article_id", null: false
   end
 
   create_table "categories", force: :cascade do |t|
