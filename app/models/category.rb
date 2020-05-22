8f830f25fa832e1b98dc34a4fe96ea5ce5c2c895
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-05-21 21:17:26 -0400

    Define many to many relationships

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,5 @@
 class Category < ApplicationRecord
+    has_and_belongs_to_many :articles
     validates :name, presence: true, length: { minimum: 3, maximum: 25 }
     validates_uniqueness_of :name
 end
